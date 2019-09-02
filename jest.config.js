4fbfad84bc371b1a1ed30f759dc93e8445d73221
Adeola Adeyemo <clasiqaas@gmail.com>
2019-09-02 12:27:49 +0100

    bg-[]: fix duplicate file and eslint errors - deletes duplicate - fix eslint errors - adds pull request template []

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
     '^store$': '<rootDir>/src/store'
   },
   transform: {
-    '^.+\\.js$': 'babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/jestConfig/__mocks__/fileTransformer.js'
   },
   moduleFileExtensions: [
